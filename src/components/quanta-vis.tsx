1b441be98d730ae81be2bcbc04f7769e8192629e
Angelo Joemon <angelojoemonk@gmail.com>
2025-08-26 16:21:28 +0000

    particles gone again

--- src/components/quanta-vis.tsx
+++ src/components/quanta-vis.tsx
@@ -343,7 +343,7 @@ const QuantaVis: React.FC = () => {
     };
   }, []);
 
-  return <div ref={mountRef} className="fixed top-0 left-0 w-full h-full -z-10" />;
+  return <div ref={mountRef} className="fixed top-0 left-0 w-full h-full" />;
 };
 
 export default QuantaVis;
