1f3822be05260900d33adc764cf14ac6ee9e4314
Angelo Joemon <angelojoemonk@gmail.com>
2025-08-27 05:07:23 +0000

    force it to use the local audio file or at least preload it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        <link rel="preload" href="/videoplayback.mp4" as="audio" type="audio/mp4" crossOrigin="anonymous" />
       </head>
       <body className="font-body antialiased">{children}</body>
     </html>
